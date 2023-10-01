8a4a7b2058236b55cfa128d27adffc5f859d3e72
Paco-Danes <devinson2001@gmail.com>
2023-10-01 19:08:32 +0200

    data exploration

--- Code/my_utils.py
+++ Code/my_utils.py
@@ -49,115 +49,6 @@ def phase_sanitization_inRange(row):
     bounded_angles[bounded_angles < 0] += 2 * np.pi
     return bounded_angles
 
-def MAD_filter(df, window_size=500, stride=None, thresh=3):
-    if stride == None:
-        stride = window_size
-    # Create an empty DataFrame to store the cleaned data
-    cleaned_df = df.copy()
-    # Loop through each column (feature) in the DataFrame
-    for column in df.columns:
-        for i in range(0, len(df) - window_size + 1, stride):
-            # Select the current window
-            window = df[column].iloc[i:i+window_size]
-            # Calculate the median and MAD for the window
-            median = window.median()
-            mad = np.median(np.abs(window - median))
-            # Define a threshold for outlier detection (e.g., 3 times MAD)
-            threshold = thresh * mad
-            # Identify outliers within the window
-            outliers = np.abs(window - median) > threshold
-            # Replace outliers with the mean of the non-outlier values in the window
-            for j in range(len(window)):
-                if outliers.iloc[j]:
-                    # Find the indices of previous and next non-outlier values within the window
-                    prev_non_outlier = j - 1
-                    next_non_outlier = j + 1
-                    while prev_non_outlier >= 0 and outliers.iloc[prev_non_outlier]:
-                        prev_non_outlier -= 1
-                    while next_non_outlier < len(window) and outliers.iloc[next_non_outlier]:
-                        next_non_outlier += 1
-                    # Replace the outlier with the mean of the non-outlier values in the window
-                    if prev_non_outlier >= 0 and next_non_outlier < len(window):
-                        cleaned_df.at[i + j, column] = (
-                            window.iloc[prev_non_outlier] + window.iloc[next_non_outlier]
-                        ) / 2
-    return cleaned_df
-
-def create_tensor(df, winsize = 200, overlap =100):
-    num_images = (len(df) - overlap) // (winsize - overlap)  # Calculate the number of images
-    # Initialize aan empty NumPy array to store the image data
-    image_data = np.empty((num_images, winsize, df.shape[1]))
-    # Create overlapping slices and populate the image data
-    for i in range(num_images):
-        start = i * (winsize - overlap)
-        end = start + winsize
-        image_slice = df.iloc[start:end, :]
-         # Store the slice data in the image_data 3D array
-        image_data[i, :, :] = image_slice.values
-    # image_data now contains your images as a 3D NumPy array (num_images, window_size, num_columns)
-    return image_data
-
-
-
-def replace_outliers_std(df, std_threshold=3, inplace=False):
-    if not inplace:
-        df = df.copy()
-    for col in df.columns:
-        mean = df[col].mean()
-        std = df[col].std()
-        # Identify outliers
-        outliers = (df[col] - mean).abs() > std_threshold * std
-        # Iterate through the DataFrame and replace outliers
-        for i in range(1, len(df) - 1):
-            if outliers.iloc[i]:
-                # Find the previous and next non-outlier indices
-                prev_index = i - 1
-                next_index = i + 1
-                while prev_index >= 0 and outliers.iloc[prev_index]:
-                    prev_index -= 1
-                while next_index < len(df) and outliers.iloc[next_index]:
-                    next_index += 1
-                # Calculate the average of previous and next non-outliers
-                avg_value = (df.iloc[prev_index][col] + df.iloc[next_index][col]) / 2
-                # Replace the outlier with the average
-                df.at[i, col] = avg_value
-    return df
-
-def replace_outliers_iqr(df):
-    # Create an empty DataFrame to store the cleaned data
-    cleaned_df = df.copy()
-    outNum = []
-    # Loop through each column (feature) in the DataFrame
-    for column in df.columns:
-        # Calculate Q1 and Q3 for the current feature
-        Q1 = df[column].quantile(0.25)
-        Q3 = df[column].quantile(0.75)
-        # Calculate the IQR for the current feature
-        IQR = Q3 - Q1
-        # Define lower and upper bounds for outlier detection
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        # Identify outliers for the current feature
-        outliers = (df[column] < lower_bound) | (df[column] > upper_bound)
-        # Replace outliers with the mean of the previous and next non-outlier values
-        for i in range(len(df)):
-            if outliers.iloc[i]:
-                # Find the indices of previous and next non-outlier values
-                prev_non_outlier = i - 1
-                next_non_outlier = i + 1
-                # Find the previous non-outlier value
-                while prev_non_outlier >= 0 and outliers.iloc[prev_non_outlier]:
-                    prev_non_outlier -= 1
-                # Find the next non-outlier value
-                while next_non_outlier < len(df) and outliers.iloc[next_non_outlier]:
-                    next_non_outlier += 1
-                # Replace the outlier with the mean of the previous and next non-outlier values
-                if prev_non_outlier >= 0 and next_non_outlier < len(df):
-                    cleaned_df.at[i, column] = (df.at[prev_non_outlier, column] + df.at[next_non_outlier, column]) / 2
-    return cleaned_df
-
-import numpy as np
-
 def hampel_filtering(df, window_size, thresh=3, smoothing_factor=0.9):
     if window_size % 2 == 0:
         raise ValueError('Window_size must be an odd number, but value provided is {}'.format(window_size))
@@ -219,8 +110,108 @@ def hampel_filtering(df, window_size, thresh=3, smoothing_factor=0.9):
     
     return cleaned_df
 
-from scipy.signal import butter, lfilter, lfilter_zi
+from scipy.stats import skew
+def add_statistics(input_df):
 
+    # Create DataFrames for each statistic
+    mean_row = pd.DataFrame(input_df.mean()).T
+    variance_row = pd.DataFrame(input_df.var()).T
+    std_row = pd.DataFrame(input_df.std()).T
+    median_row = pd.DataFrame(input_df.median()).T
+    skewness_row = pd.DataFrame(input_df.apply(lambda x: skew(x, nan_policy='omit'))).T
+
+    # Concatenate the statistics DataFrames to the input DataFrame
+    result_df = pd.concat([input_df, mean_row, variance_row , std_row, median_row, skewness_row], ignore_index=True)
+
+    mean_col = result_df.mean(axis=1)
+    variance_col = result_df.var(axis=1)
+    std_col = result_df.std(axis=1)
+    median_col = result_df.median(axis=1)
+    skewness_col = result_df.apply(lambda x: skew(x, nan_policy='omit'), axis=1)
+
+    #result_df['Mean'] = mean_col
+    result_df['Variance'] = variance_col
+    result_df['Std'] = std_col
+    result_df['Median'] = median_col
+    result_df['Skewness'] = skewness_col
+
+    return result_df
+
+def create_tensor(df, winsize = 200, overlap =100):
+    num_images = (len(df) - overlap) // (winsize - overlap)  # Calculate the number of images
+    # Initialize aan empty NumPy array to store the image data
+    image_data = np.empty((num_images, winsize, df.shape[1]))
+    # Create overlapping slices and populate the image data
+    for i in range(num_images):
+        start = i * (winsize - overlap)
+        end = start + winsize
+        image_slice = df.iloc[start:end, :]
+         # Store the slice data in the image_data 3D array
+        image_data[i, :, :] = image_slice.values
+    # image_data now contains your images as a 3D NumPy array (num_images, window_size, num_columns)
+    return image_data
+
+
+
+
+def replace_outliers_std(df, std_threshold=3, inplace=False):
+    if not inplace:
+        df = df.copy()
+    for col in df.columns:
+        mean = df[col].mean()
+        std = df[col].std()
+        # Identify outliers
+        outliers = (df[col] - mean).abs() > std_threshold * std
+        # Iterate through the DataFrame and replace outliers
+        for i in range(1, len(df) - 1):
+            if outliers.iloc[i]:
+                # Find the previous and next non-outlier indices
+                prev_index = i - 1
+                next_index = i + 1
+                while prev_index >= 0 and outliers.iloc[prev_index]:
+                    prev_index -= 1
+                while next_index < len(df) and outliers.iloc[next_index]:
+                    next_index += 1
+                # Calculate the average of previous and next non-outliers
+                avg_value = (df.iloc[prev_index][col] + df.iloc[next_index][col]) / 2
+                # Replace the outlier with the average
+                df.at[i, col] = avg_value
+    return df
+
+def replace_outliers_iqr(df):
+    # Create an empty DataFrame to store the cleaned data
+    cleaned_df = df.copy()
+    outNum = []
+    # Loop through each column (feature) in the DataFrame
+    for column in df.columns:
+        # Calculate Q1 and Q3 for the current feature
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        # Calculate the IQR for the current feature
+        IQR = Q3 - Q1
+        # Define lower and upper bounds for outlier detection
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        # Identify outliers for the current feature
+        outliers = (df[column] < lower_bound) | (df[column] > upper_bound)
+        # Replace outliers with the mean of the previous and next non-outlier values
+        for i in range(len(df)):
+            if outliers.iloc[i]:
+                # Find the indices of previous and next non-outlier values
+                prev_non_outlier = i - 1
+                next_non_outlier = i + 1
+                # Find the previous non-outlier value
+                while prev_non_outlier >= 0 and outliers.iloc[prev_non_outlier]:
+                    prev_non_outlier -= 1
+                # Find the next non-outlier value
+                while next_non_outlier < len(df) and outliers.iloc[next_non_outlier]:
+                    next_non_outlier += 1
+                # Replace the outlier with the mean of the previous and next non-outlier values
+                if prev_non_outlier >= 0 and next_non_outlier < len(df):
+                    cleaned_df.at[i, column] = (df.at[prev_non_outlier, column] + df.at[next_non_outlier, column]) / 2
+    return cleaned_df
+
+from scipy.signal import butter, lfilter, lfilter_zi
 def butterworth_filter(df, order, cutoff_frequency, sampling_rate):
     """
     Apply a Butterworth filter to each column of a DataFrame.
